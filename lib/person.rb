4ade46893c8bb6d418d5b4e3321b0c151b22dda2
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 01:01:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
 
   def initialize (:name, :birthday, :hair_color, :eye_color, :heigh)
-    @name = name 
+    @name = name
     @birthday = birthday
     @hair_color = hair_color
     @eye_color = eye_color
