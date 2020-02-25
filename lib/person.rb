890ead250e86f752d291e8dd7ac04c8f42375f2b
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 01:14:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh
 
 
-  def initialize (name:,birthday:,hair_color:, eye_color:, height:)
+  def initialize (name:, birthday:, hair_color:, eye_color:, height:)
     @name = :name
     @birthday = :birthday
     @hair_color = :hair_color
