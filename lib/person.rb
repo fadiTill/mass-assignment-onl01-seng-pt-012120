a047e8822a8217a9ed1f4dfac195fdb2257a9319
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 01:18:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
+
 class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh
 
 
-  
+
     def initialize(attributes)
       attributes.each {|key, value| self.send(("#{key}="), value)}
     end
   end
 end
-
