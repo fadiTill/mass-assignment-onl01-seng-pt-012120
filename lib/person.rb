ee10b7fa282b18152ca3da7a247ff08182af42c7
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 01:16:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,10 @@ class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :heigh
 
 
-  def initialize (name:, birthday:, hair_color:, eye_color:, height:)
-    @name = :name
-    @birthday = :birthday
-    @hair_color = :hair_color
-    @eye_color = :eye_color
-    @height = :height
-  #your code here
+  
+    def initialize(attributes)
+      attributes.each {|key, value| self.send(("#{key}="), value)}
+    end
+  end
 end
 end
